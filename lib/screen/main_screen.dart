72915f9f4b5dd7369de943e719c30aec41fb2e3a
untillness <pvauntil@gmail.com>
2020-11-18 13:57:08 +0700

    improve: refactor code

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -13,147 +13,10 @@ class MainScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Stack(
           children: [
-            Container(
-              width: Get.width,
-              height: Get.height,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: [
-                    Color(0xFF48c6ef),
-                    Color(0xFF6f86d6),
-                  ],
-                ),
-              ),
-            ),
-            Positioned.fill(
-              bottom: Get.pixelRatio * 15,
-              child: Align(
-                alignment: Alignment.center,
-                child: Text(
-                  'WA.ME PLEASE.',
-                  style: GoogleFonts.squadaOne(
-                    fontSize: Get.pixelRatio * 18,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-            Positioned.fill(
-              bottom: Get.pixelRatio * 90,
-              child: Align(
-                alignment: Alignment.center,
-                child: Container(
-                  height: 200,
-                  width: 300,
-                  child: Lottie.asset('assets/lottie/chat.json'),
-                ),
-              ),
-            ),
-            Positioned.fill(
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: Container(
-                  width: Get.width,
-                  height: Get.height / 3,
-                  decoration: BoxDecoration(
-                    color: Color(0xFFd3daf2),
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(Get.pixelRatio * 6),
-                      topRight: Radius.circular(Get.pixelRatio * 6),
-                    ),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Color(0xFF6378c0),
-                        blurRadius: 20,
-                        // spreadRadius: ,
-                      )
-                    ],
-                  ),
-                  padding: EdgeInsets.all(Get.pixelRatio * 8),
-                  child: Column(
-                    children: [
-                      Container(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: Get.pixelRatio * 3,
-                          vertical: Get.pixelRatio / 100,
-                        ),
-                        decoration: BoxDecoration(
-                          color: Color(0xFF6f86d6),
-                          borderRadius: BorderRadius.circular(
-                            Get.pixelRatio * 3,
-                          ),
-                        ),
-                        child: TextField(
-                          controller: mainController.textEditingControllerNo,
-                          inputFormatters: [mainController.textMaskTelephone],
-                          keyboardType: TextInputType.number,
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            labelText: 'Masukkan Nomer WhatsApp',
-                            labelStyle: GoogleFonts.roboto(
-                              fontSize: Get.pixelRatio * 5,
-                              color: Colors.white,
-                            ),
-                          ),
-                          style: GoogleFonts.roboto(
-                            fontSize: Get.pixelRatio * 9,
-                            color: Colors.white,
-                          ),
-                          cursorColor: Colors.white,
-                        ),
-                      ),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: Padding(
-                              padding: EdgeInsets.only(right: Get.pixelRatio),
-                              child: RaisedButton(
-                                color: Color(0xFF6f86d6),
-                                onPressed: () => {},
-                                child: Text(
-                                  'Scan QR',
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(
-                                    Get.pixelRatio * 3,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                          Expanded(
-                            child: Padding(
-                              padding: EdgeInsets.only(left: Get.pixelRatio),
-                              child: RaisedButton(
-                                color: Color(0xFF6f86d6),
-                                onPressed: () => {
-                                  mainController.waNow(
-                                      nomer: mainController.textMaskTelephone
-                                          .getUnmaskedText())
-                                },
-                                child: Text(
-                                  'WA NOW!',
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(
-                                    Get.pixelRatio * 3,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
+            BackgroundGradient(),
+            TitleApp(),
+            AnimationImage(),
+            MainContent(mainController: mainController),
             SlidingUpPanel(
               controller: mainController.panelController,
               renderPanelSheet: false,
@@ -161,59 +24,268 @@ class MainScreen extends StatelessWidget {
               minHeight: Get.pixelRatio * 17,
               backdropEnabled: true,
               backdropTapClosesPanel: true,
-              panel: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius:
-                      BorderRadius.all(Radius.circular(Get.pixelRatio * 6)),
-                ),
-                margin: EdgeInsets.fromLTRB(Get.pixelRatio * 4, 0,
-                    Get.pixelRatio * 4, Get.pixelRatio * 4),
-                child: Center(
-                  child: Text("This is the SlidingUpPanel when open"),
+              borderRadius: _buildBorderRadiusPanel(),
+              collapsed: HeaderPanel(mainController: mainController),
+              panel: ContentPanel(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ContentPanel extends StatelessWidget {
+  const ContentPanel({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius:
+            BorderRadius.all(Radius.circular(Get.pixelRatio * 6)),
+      ),
+      margin: EdgeInsets.fromLTRB(Get.pixelRatio * 4, 0,
+          Get.pixelRatio * 4, Get.pixelRatio * 4),
+      child: Center(
+        child: Text("This is the SlidingUpPanel when open"),
+      ),
+    );
+  }
+}
+
+class HeaderPanel extends StatelessWidget {
+  const HeaderPanel({
+    Key key,
+    @required this.mainController,
+  }) : super(key: key);
+
+  final MainController mainController;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => mainController.panelController.open(),
+      child: Container(
+        margin: EdgeInsets.fromLTRB(
+            Get.pixelRatio * 4, 0, Get.pixelRatio * 4, 0),
+        decoration: BoxDecoration(
+            color: Color(0xFF6f86d6),
+            borderRadius: _buildBorderRadiusPanel()),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: Get.width / (Get.pixelRatio / 2),
+              height: Get.pixelRatio * 2,
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(.7),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(100),
                 ),
               ),
-              collapsed: GestureDetector(
-                onTap: () => mainController.panelController.open(),
-                child: Container(
-                  margin: EdgeInsets.fromLTRB(
-                      Get.pixelRatio * 4, 0, Get.pixelRatio * 4, 0),
-                  decoration: BoxDecoration(
-                    color: Color(0xFF6f86d6),
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(Get.pixelRatio * 6),
-                      topRight: Radius.circular(Get.pixelRatio * 6),
+            ),
+            SizedBox(
+              height: Get.pixelRatio * 3,
+            ),
+            Text(
+              'Histori nge-WA mu',
+              style: GoogleFonts.roboto(color: Colors.white),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+BorderRadius _buildBorderRadiusPanel() {
+  return BorderRadius.only(
+    topLeft: Radius.circular(Get.pixelRatio * 6),
+    topRight: Radius.circular(Get.pixelRatio * 6),
+  );
+}
+
+class MainContent extends StatelessWidget {
+  const MainContent({
+    Key key,
+    @required this.mainController,
+  }) : super(key: key);
+
+  final MainController mainController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned.fill(
+      child: Align(
+        alignment: Alignment.bottomCenter,
+        child: Container(
+          width: Get.width,
+          height: Get.height / 3,
+          decoration: BoxDecoration(
+            color: Color(0xFFd3daf2),
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(Get.pixelRatio * 6),
+              topRight: Radius.circular(Get.pixelRatio * 6),
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Color(0xFF6378c0),
+                blurRadius: 20,
+                // spreadRadius: ,
+              )
+            ],
+          ),
+          padding: EdgeInsets.all(Get.pixelRatio * 4),
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: Get.pixelRatio * 3,
+                  vertical: Get.pixelRatio / 100,
+                ),
+                decoration: BoxDecoration(
+                  color: Color(0xFF6f86d6),
+                  borderRadius: BorderRadius.circular(
+                    Get.pixelRatio * 3,
+                  ),
+                ),
+                child: TextField(
+                  controller: mainController.textEditingControllerNo,
+                  inputFormatters: [mainController.textMaskTelephone],
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    labelText: 'Masukkan Nomer WhatsApp',
+                    labelStyle: GoogleFonts.roboto(
+                      fontSize: Get.pixelRatio * 5,
+                      color: Colors.white,
                     ),
                   ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        width: Get.width / (Get.pixelRatio / 2),
-                        height: Get.pixelRatio * 2,
-                        decoration: BoxDecoration(
-                          color: Colors.white.withOpacity(.7),
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(100),
+                  style: GoogleFonts.roboto(
+                    fontSize: Get.pixelRatio * 9,
+                    color: Colors.white,
+                  ),
+                  cursorColor: Colors.white,
+                ),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.only(right: Get.pixelRatio),
+                      child: RaisedButton(
+                        color: Color(0xFF6f86d6),
+                        onPressed: () => {},
+                        child: Text(
+                          'Scan QR',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                            Get.pixelRatio * 3,
                           ),
                         ),
                       ),
-                      SizedBox(
-                        height: Get.pixelRatio * 3,
-                      ),
-                      Text(
-                        'Histori nge-WA mu',
-                        style: GoogleFonts.roboto(color: Colors.white),
-                      )
-                    ],
+                    ),
                   ),
-                ),
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.only(left: Get.pixelRatio),
+                      child: RaisedButton(
+                        color: Color(0xFF6f86d6),
+                        onPressed: () => {
+                          mainController.waNow(
+                              nomer: mainController.textMaskTelephone
+                                  .getUnmaskedText())
+                        },
+                        child: Text(
+                          'WA NOW!',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                            Get.pixelRatio * 3,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(Get.pixelRatio * 6),
-                topRight: Radius.circular(Get.pixelRatio * 6),
-              ),
-            ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class AnimationImage extends StatelessWidget {
+  const AnimationImage({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned.fill(
+      bottom: Get.pixelRatio * 90,
+      child: Align(
+        alignment: Alignment.center,
+        child: Container(
+          height: 200,
+          width: 300,
+          child: Lottie.asset('assets/lottie/chat.json'),
+        ),
+      ),
+    );
+  }
+}
+
+class TitleApp extends StatelessWidget {
+  const TitleApp({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned.fill(
+      bottom: Get.pixelRatio * 15,
+      child: Align(
+        alignment: Alignment.center,
+        child: Text(
+          'WA.ME PLEASE.',
+          style: GoogleFonts.squadaOne(
+            fontSize: Get.pixelRatio * 18,
+            fontWeight: FontWeight.w400,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BackgroundGradient extends StatelessWidget {
+  const BackgroundGradient({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: Get.width,
+      height: Get.height,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Color(0xFF48c6ef),
+            Color(0xFF6f86d6),
           ],
         ),
       ),
